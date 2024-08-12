17993136193401f811675721eb7b90c1ea4caa89
NguyenVu <nmv597nmv@gmail.com>
2024-08-13 03:55:11 +0700

    Added my project

---
+++ set_env.bat
@@ -0,0 +1,10 @@
+
+setx POSTGRES_USERNAME postgres
+setx POSTGRES_PASSWORD postgres
+setx POSTGRES_HOST microservice.ce6ir2ndsxp4.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB postgres
+setx AWS_BUCKET arn:aws:s3:::refactor-monolith-microservice
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET testing
+setx URL http://localhost:8100
